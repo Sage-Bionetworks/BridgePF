32818d703276cd992e5a4f4fd4e4fb4bcc87daaf
eric-wu <eric.wu@sagebase.org>
2014-04-30 01:37:38 -0700

    Parallelize the tests.

---
+++ travis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ev
+if [ "${TARGET}" -eq "JavaScript"]
+    then
+        export JS_RELEASED=false
+        npm install bower
+        npm install grunt-cli
+        gem install sass
+        cd public
+        npm install
+        bower install
+        grunt test
+        grunt release
+        cd ..
+        export JS_RELEASED=true
+    else
+        sbt test
+fi
