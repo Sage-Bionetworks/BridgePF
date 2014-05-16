8166fe494efcbf91b69dd59d71fc4f20bea044a5
eric-wu <eric.wu@sagebase.org>
2014-05-16 11:59:12 -0700

    Make stub the default environment.

---
+++ app/org/sagebionetworks/bridge/config/Environment.java
@@ -0,0 +1,19 @@
+package org.sagebionetworks.bridge.config;
+
+public enum Environment {
+
+    STUB("stub"),
+    LOCAL("local"),
+    DEV("dev"),
+    PROD("prod");
+
+    public String getEnvName() {
+        return name;
+    }
+
+    private Environment(String name) {
+        this.name = name;
+    }
+
+    private final String name;
+}
