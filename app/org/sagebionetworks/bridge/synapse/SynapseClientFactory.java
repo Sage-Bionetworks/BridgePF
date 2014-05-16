98392492fa94f59c76e43bc98a2f6b2149fc40e3
alxdarksage <alx.dark@sagebase.org>
2014-05-16 10:25:46 -0700

    Removing application-context-per-environment and configuring through BridgeConfig

---
+++ app/org/sagebionetworks/bridge/synapse/SynapseClientFactory.java
@@ -0,0 +1,22 @@
+package org.sagebionetworks.bridge.synapse;
+
+import org.sagebionetworks.bridge.config.BridgeConfig;
+import org.sagebionetworks.client.SynapseClient;
+import org.sagebionetworks.client.SynapseClientImpl;
+import org.sagebionetworks.repo.model.DomainType;
+
+public class SynapseClientFactory {
+
+    public static SynapseClient createSynapseClient(String repoEndpoint, String authEndpoint, String fileEndpoint) {
+        if (new BridgeConfig().isStub()) {
+            return StubSynapseClient.createInstance();            
+        } else {
+            SynapseClient client = new SynapseClientImpl(DomainType.BRIDGE);
+            client.setRepositoryEndpoint(repoEndpoint);
+            client.setAuthEndpoint(authEndpoint);
+            client.setFileEndpoint(fileEndpoint);
+            return client;
+        }
+    }
+    
+}
