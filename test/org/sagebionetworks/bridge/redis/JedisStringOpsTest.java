acc838c1632a11e5078f7769f7ae684d5eea436b
eric-wu <eric.wu@sagebase.org>
2014-06-04 16:17:03 -0700

    Test expire.

--- test/org/sagebionetworks/bridge/redis/JedisStringOpsTest.java
+++ test/org/sagebionetworks/bridge/redis/JedisStringOpsTest.java
@@ -1,6 +1,7 @@
 package org.sagebionetworks.bridge.redis;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
@@ -8,7 +9,9 @@ public class JedisStringOpsTest {
     @Test
     public void test() throws Exception {
         StringOps strOps = new JedisStringOps();
-        assertEquals("OK", strOps.setex("testKey", 10, "testValue").execute());
+        assertEquals("OK", strOps.setex("testKey", 2, "testValue").execute());
         assertEquals("testValue", strOps.get("testKey").execute());
+        Thread.sleep(2000);
+        assertNull(strOps.get("testKey").execute());
     }
 }
