da22d24229f99109bc0e1ea337c78e531006c726
eric-wu <eric.wu@sagebase.org>
2014-06-13 00:49:45 -0700

    Separate out test configuration file.

--- test/org/sagebionetworks/bridge/config/BridgeConfigTest.java
+++ test/org/sagebionetworks/bridge/config/BridgeConfigTest.java
@@ -3,11 +3,15 @@ package org.sagebionetworks.bridge.config;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+import java.io.File;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class BridgeConfigTest {
+
+    private static final File CONF_FILE = new File("test/conf/bridge.conf");
 
     @Before
     public void before() {
@@ -27,13 +31,13 @@ public class BridgeConfigTest {
 
     @Test
     public void testNonExisting() {
-        BridgeConfig config = new BridgeConfig();
+        BridgeConfig config = new BridgeConfig(CONF_FILE);
         assertNull(config.getProperty("someFakePropertyThatDoesNotExist"));
     }
 
     @Test
     public void testEncryption() {
-        BridgeConfig config = new BridgeConfig();
+        BridgeConfig config = new BridgeConfig(CONF_FILE);
         assertEquals("example.value", config.getProperty("example.property"));
         assertEquals("example.value.encrypted", config.getProperty("example.property.encrypted"));
     }
@@ -41,19 +45,19 @@ public class BridgeConfigTest {
     @Test
     public void testEnvironment() {
         System.setProperty("bridge.env", "dev");
-        BridgeConfig config = new BridgeConfig();
+        BridgeConfig config = new BridgeConfig(CONF_FILE);
         assertEquals("example.value.for.dev", config.getProperty("example.property"));
     }
 
     @Test
     public void testUser() {
-        BridgeConfig config = new BridgeConfig();
+        BridgeConfig config = new BridgeConfig(CONF_FILE);
         assertEquals("unit.test", config.getUser());
     }
 
     @Test
     public void testNumber() {
-        BridgeConfig config = new BridgeConfig();
+        BridgeConfig config = new BridgeConfig(CONF_FILE);
         assertEquals(2000, config.getPropertyAsInt("example.timeout"));
     }
 }
