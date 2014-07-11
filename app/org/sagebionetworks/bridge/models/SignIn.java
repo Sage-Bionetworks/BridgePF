d15876b870c083fad350ebc042f1cae4f1f6cde7
eric-wu <eric.wu@sagebase.org>
2014-07-11 11:52:14 -0700

    Replace tabs with spaces -- round 1.

--- app/org/sagebionetworks/bridge/models/SignIn.java
+++ app/org/sagebionetworks/bridge/models/SignIn.java
@@ -6,38 +6,38 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 public final class SignIn {
 
-	private static final String PASSWORD_FIELD = "password";
-	private static final String USERNAME_FIELD = "username";
-	
-	private final String username;
-	private final String password;
-	
-	public SignIn(String username, String password) {
-		this.username = username;
-		this.password = password;
-	}
-	
-	public static final SignIn fromJson(JsonNode node) {
-		String username = null;
-		String password = null;
-		if (node != null && node.get(USERNAME_FIELD) != null) {
-			username = node.get(USERNAME_FIELD).asText();
-		}
-		if (node != null && node.get(PASSWORD_FIELD) != null) {
-			password = node.get(PASSWORD_FIELD).asText();
-		}
-		return new SignIn(username, password);
-	}
+    private static final String PASSWORD_FIELD = "password";
+    private static final String USERNAME_FIELD = "username";
+    
+    private final String username;
+    private final String password;
+    
+    public SignIn(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
+    public static final SignIn fromJson(JsonNode node) {
+        String username = null;
+        String password = null;
+        if (node != null && node.get(USERNAME_FIELD) != null) {
+            username = node.get(USERNAME_FIELD).asText();
+        }
+        if (node != null && node.get(PASSWORD_FIELD) != null) {
+            password = node.get(PASSWORD_FIELD).asText();
+        }
+        return new SignIn(username, password);
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public String getPassword() {
-		return password;
-	}
-	
-	public boolean isBlank() {
-		return StringUtils.isBlank(username) && StringUtils.isBlank(password);
-	}
+    public String getPassword() {
+        return password;
+    }
+    
+    public boolean isBlank() {
+        return StringUtils.isBlank(username) && StringUtils.isBlank(password);
+    }
 }
--- app/org/sagebionetworks/bridge/models/SignUp.java
+++ app/org/sagebionetworks/bridge/models/SignUp.java
@@ -4,46 +4,46 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 public class SignUp {
 
-	private static final String EMAIL_FIELD = "email";
-	private static final String USERNAME_FIELD = "username";
-	private static final String PASSWORD_FIELD = "password";
-	
-	private final String username;
-	private final String email;
-	private final String password;
-	
-	public SignUp(String username, String email, String password) {
-		this.username = username;
-		this.email = email;
-		this.password = password;
-	}
-	
-	public static SignUp fromJson(JsonNode node) {
-		String username = null;
-		String email = null;
-		String password = null;
-		if (node != null && node.get(USERNAME_FIELD) != null) {
-			username = node.get(USERNAME_FIELD).asText();
-		}
-		if (node != null && node.get(EMAIL_FIELD) != null) {
-			email = node.get(EMAIL_FIELD).asText();
-		}
+    private static final String EMAIL_FIELD = "email";
+    private static final String USERNAME_FIELD = "username";
+    private static final String PASSWORD_FIELD = "password";
+    
+    private final String username;
+    private final String email;
+    private final String password;
+    
+    public SignUp(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    
+    public static SignUp fromJson(JsonNode node) {
+        String username = null;
+        String email = null;
+        String password = null;
+        if (node != null && node.get(USERNAME_FIELD) != null) {
+            username = node.get(USERNAME_FIELD).asText();
+        }
+        if (node != null && node.get(EMAIL_FIELD) != null) {
+            email = node.get(EMAIL_FIELD).asText();
+        }
         if (node != null && node.get(PASSWORD_FIELD) != null) {
             password = node.get(PASSWORD_FIELD).asText();
         }
-		return new SignUp(username, email, password);
-	}
+        return new SignUp(username, email, password);
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public String getEmail() {
-		return email;
-	}
-	
-	public String getPassword() {
-	    return password;
-	}
+    public String getEmail() {
+        return email;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
 
 }
