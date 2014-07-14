d261111e7e388df7df7c4497f19e11071b425330
alxdarksage <alx.dark@sagebase.org>
2014-07-14 10:40:18 -0700

    Moving wizard back to welcome screen

--- public/consent/scripts/consent.js
+++ public/consent/scripts/consent.js
@@ -31,7 +31,7 @@ angular.module('consent', ['bridge.shared']).controller('ConsentController',
         }
     };
 
-    $scope.setStep("thankyou");
+    $scope.setStep("welcome");
     
     /* TASKS */
     /* -------------------------------------------------------------------- */
