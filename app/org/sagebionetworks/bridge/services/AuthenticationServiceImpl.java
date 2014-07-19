e1c0dfc85efa28bc39b80d4d7d14c0938d52cd18
eric-wu <eric.wu@sagebase.org>
2014-07-19 09:48:10 -0700

    Remove code that does insufficient encryption.

--- app/org/sagebionetworks/bridge/services/AuthenticationServiceImpl.java
+++ app/org/sagebionetworks/bridge/services/AuthenticationServiceImpl.java
@@ -3,14 +3,12 @@ package org.sagebionetworks.bridge.services;
 import java.util.UUID;
 
 import org.apache.commons.httpclient.HttpStatus;
-import org.jasypt.encryption.pbe.PBEStringEncryptor;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.validator.routines.EmailValidator;
 import org.sagebionetworks.bridge.BridgeConstants;
 import org.sagebionetworks.bridge.cache.CacheProvider;
 import org.sagebionetworks.bridge.config.BridgeConfig;
 import org.sagebionetworks.bridge.crypto.BridgeEncryptor;
-import org.sagebionetworks.bridge.crypto.EncryptorUtil;
 import org.sagebionetworks.bridge.exceptions.BridgeNotFoundException;
 import org.sagebionetworks.bridge.exceptions.BridgeServiceException;
 import org.sagebionetworks.bridge.exceptions.ConsentRequiredException;
@@ -24,8 +22,6 @@ import org.sagebionetworks.bridge.models.Study;
 import org.sagebionetworks.bridge.models.UserSession;
 import org.sagebionetworks.bridge.models.UserSessionInfo;
 import org.sagebionetworks.bridge.stormpath.StormpathFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.stormpath.sdk.account.Account;
 import com.stormpath.sdk.application.Application;
@@ -38,8 +34,6 @@ import com.stormpath.sdk.directory.Directory;
 import com.stormpath.sdk.resource.ResourceException;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
-
-    private static Logger logger = LoggerFactory.getLogger(AuthenticationServiceImpl.class);
 
     private Client stormpathClient;
     private CacheProvider cache;
@@ -94,7 +88,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         UserSession session = null;
         try {
             
-            Application application = StormpathFactory.createStormpathApplication(stormpathClient); 
+            Application application = StormpathFactory.createStormpathApplication(stormpathClient);
             request = new UsernamePasswordRequest(signIn.getUsername(), signIn.getPassword());
             Account account = application.authenticateAccount(request).getAccount();
             
@@ -238,6 +232,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     }
 
     private UserSession createSessionFromAccount(Study study, Account account) {
+
         UserSession session;
         session = new UserSession();
         session.setAuthenticated(true);
@@ -249,15 +244,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
         CustomData data = account.getCustomData();
         String consentKey = study.getKey()+BridgeConstants.CUSTOM_DATA_CONSENT_SUFFIX;
-        session.setConsent( "true".equals(data.get(consentKey)) );
-        final String hdcKey = study.getKey()+BridgeConstants.CUSTOM_DATA_HEALTH_CODE_SUFFIX;
-        final String encryptedId = (String)data.get(hdcKey);
-        Object version = data.get(BridgeConstants.CUSTOM_DATA_VERSION);
-        if (version == null) {
-            PBEStringEncryptor encryptor = EncryptorUtil.getEncryptorOld(
-                    config.getHealthCodePassword(), config.getHealthCodeSalt());
-            session.setHealthDataCode(encryptor.decrypt(encryptedId));
-        } else {
+        final boolean consented = "true".equals(data.get(consentKey));
+        session.setConsent(consented);
+        
+        if (consented) {
+            final String hdcKey = study.getKey()+BridgeConstants.CUSTOM_DATA_HEALTH_CODE_SUFFIX;
+            final String encryptedId = (String)data.get(hdcKey);
             String healthId = healthCodeEncryptor.decrypt(encryptedId);
             String healthCode = healthCodeService.getHealthCode(healthId);
             session.setHealthDataCode(healthCode);
