c3445878273cad277c3ec03ecf65e7fb9dc47911
Eric Wu <eric.wu@sagebase.org>
2014-08-18 13:22:25 -0700

    Add package-info to the dynamodb.test package.

---
+++ test/org/sagebionetworks/bridge/dynamodb/test/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package is for testing DynamoInitializer so that, during testing,
+ * only DynamoDB tables defined in this package are initialized.
+ */
+package org.sagebionetworks.bridge.dynamodb.test;
