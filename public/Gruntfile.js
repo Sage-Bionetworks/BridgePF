bd4c73b8ba6b0ad5d47c4ad5b002005573d37aa5
alxdarksage <alx.dark@sagebase.org>
2014-08-22 16:48:06 -0700

    Changing build

--- public/Gruntfile.js
+++ public/Gruntfile.js
@@ -9,15 +9,17 @@ module.exports = function(grunt) {
             all: {
                 src: ['shared/Gruntfile.js', 'app/Gruntfile.js', 'neurod/Gruntfile.js', 'consent/Gruntfile.js'],
                 tasks: ['test','build','default','release','watch','clean']
-            },
+            }
         },
+        clean: ['node_modules']
     });
 
     grunt.loadNpmTasks('grunt-hub');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.registerTask('test', ['hub:all:test']);
     grunt.registerTask('build', ['hub:all:build']);
     grunt.registerTask('default', ['hub:all:default']);
-    grunt.registerTask('release', ['hub:all:release']);
+    grunt.registerTask('release', ['hub:all:release', 'clean']);
     grunt.registerTask('watch', ['hub:all:watch']);
-    grunt.registerTask('clean', ['hub:all:clean']);
+    grunt.registerTask('clean-all', ['hub:all:clean']);
 };
