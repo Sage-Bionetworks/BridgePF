2aa3b8a627a13c06476b52f1f73f6516ffcc0444
Eric Wu <eric.wu@sagebase.org>
2014-09-15 18:13:05 -0700

    Upload service.

--- app/org/sagebionetworks/bridge/services/UploadServiceImpl.java
+++ app/org/sagebionetworks/bridge/services/UploadServiceImpl.java
@@ -10,59 +10,43 @@ import org.sagebionetworks.bridge.config.BridgeConfigFactory;
 
 import com.amazonaws.HttpMethod;
 import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicSessionCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
-import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClient;
-import com.amazonaws.services.securitytoken.model.Credentials;
-import com.amazonaws.services.securitytoken.model.GetSessionTokenRequest;
-import com.amazonaws.services.securitytoken.model.GetSessionTokenResult;
 
 public class UploadServiceImpl implements UploadService {
 
-    private static final Integer TOKEN_EXPIRE = Integer.valueOf(900); // 15 minutes
-    private static final long UPLOAD_EXPIRE = 60 * 1000; // 1 minute
-    private static final String BUCKET = BridgeConfigFactory.getConfig().getProperty("upload.bucket");
+    private static final long EXPIRATION = 60000 * 1000; // 1 minute
+    private static final String BUCKET = BridgeConfigFactory.getConfig().getProperty("upload.bucket.pd");
 
-    private AWSSecurityTokenServiceClient tokenServiceClient;
+    private UploadSessionCredentialsService uploadCredentailsService;
     private AmazonS3 s3UploadClient;
+    private AmazonS3 s3Client;
 
-    public void setTokenServiceClient(AWSSecurityTokenServiceClient tokenServiceClient) {
-        this.tokenServiceClient = tokenServiceClient;
+    public void setUploadSessionCredentialsService(UploadSessionCredentialsService uploadCredentialsService) {
+        this.uploadCredentailsService = uploadCredentialsService;
     }
     public void setS3UploadClient(AmazonS3 s3UploadClient) {
         this.s3UploadClient = s3UploadClient;
     }
+    public void setS3Client(AmazonS3 s3Client) {
+        this.s3Client = s3Client;
+    }
 
     @Override
     public URL createUpload() {
-
-        // Create temporary credentials
-        GetSessionTokenRequest getSessionTokenRequest = new GetSessionTokenRequest();
-        getSessionTokenRequest.setDurationSeconds(TOKEN_EXPIRE);
-        GetSessionTokenResult sessionTokenResult = tokenServiceClient.getSessionToken(getSessionTokenRequest);
-        Credentials sessionCredentials = sessionTokenResult.getCredentials();
-        final AWSCredentials credentials = new BasicSessionCredentials(
-                sessionCredentials.getAccessKeyId(),
-                sessionCredentials.getSecretAccessKey(),
-                sessionCredentials.getSessionToken());
-
-        // Compute upload expiration
+        final AWSCredentials credentials = uploadCredentailsService.getSessionCredentials();
         final Date expiration = DateTime.now(DateTimeZone.UTC).toDate();
-        expiration.setTime(expiration.getTime() + UPLOAD_EXPIRE);
-
-        String id = UUID.randomUUID().toString();
+        expiration.setTime(expiration.getTime() + EXPIRATION);
         GeneratePresignedUrlRequest presignedUrlRequest = 
-                new GeneratePresignedUrlRequest(BUCKET, id);
+                new GeneratePresignedUrlRequest(BUCKET, UUID.randomUUID().toString());
         presignedUrlRequest.setMethod(HttpMethod.PUT); 
         presignedUrlRequest.setExpiration(expiration);
         presignedUrlRequest.setRequestCredentials(credentials);
-        presignedUrlRequest.setContentMd5("cc9982c0879594647c3fd05eb9534097");
-        presignedUrlRequest.setContentType("text/plain");
         return s3UploadClient.generatePresignedUrl(presignedUrlRequest);
     }
 
     @Override
-    public void uploadComplete(String id) {
+    public void uploadComplete(String key) {
+        s3Client.getObjectMetadata(BUCKET, key);
     }
 }
--- test/org/sagebionetworks/bridge/services/UploadServiceTest.java
+++ test/org/sagebionetworks/bridge/services/UploadServiceTest.java
@@ -1,16 +1,26 @@
 package org.sagebionetworks.bridge.services;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.sagebionetworks.bridge.config.BridgeConfigFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.amazonaws.services.s3.AmazonS3;
 
 @ContextConfiguration("classpath:test-context.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -19,24 +29,46 @@ public class UploadServiceTest {
     @Resource
     private UploadService uploadService;
 
+    @Resource
+    private AmazonS3 s3Client;
+
+    private List<String> objectsToRemove;
+
+    @Before
+    public void before() {
+        assertNotNull(uploadService);
+        assertNotNull(s3Client);
+        objectsToRemove = new ArrayList<String>();
+    }
+
+    @After
+    public void after() {
+        String bucket = BridgeConfigFactory.getConfig().getProperty("upload.bucket.pd");
+        for (String obj : objectsToRemove) {
+            s3Client.deleteObject(bucket, obj);
+        }
+    }
+
     @Test
     public void test() throws Exception {
         URL url = uploadService.createUpload();
-        System.out.println(url.toString());
-        upload(url);
+        assertEquals(200, upload(url));
+        String uploadId = url.getPath();
+        uploadId = uploadId.substring(1); // Remove the leading '/'
+        uploadService.uploadComplete(uploadId);
+        objectsToRemove.add(uploadId);
     }
 
-    private void upload(URL url) throws IOException {
+    private int upload(URL url) throws IOException {
         HttpURLConnection conn = (HttpURLConnection)url.openConnection();
         conn.setRequestMethod("PUT");
         conn.setDoOutput(true);
-        conn.setRequestProperty("Content-MD5", "cc9982c0879594647c3fd05eb9534097");
-        conn.setRequestProperty("Content-Type", "text/plain");
         OutputStreamWriter out = new OutputStreamWriter(
                 conn.getOutputStream());
         out.write("This text uploaded as object.");
         out.close();
-        System.out.println(conn.getResponseMessage());
-        System.out.println(conn.getResponseCode());
+        int responseCode = conn.getResponseCode();
+        conn.disconnect();
+        return responseCode;
     }
 }
