095b7a3714fdb4a7ebe401dd2682def8a42df32f
Eric Wu <eric.wu@sagebase.org>
2014-09-23 17:44:18 -0700

    Add test for the upload dao.

--- app/org/sagebionetworks/bridge/dao/UploadDao.java
+++ app/org/sagebionetworks/bridge/dao/UploadDao.java
@@ -5,9 +5,21 @@ import org.sagebionetworks.bridge.models.UploadRequest;
 public interface UploadDao {
 
     /**
+     * Creates a new upload.
+     *
      * @return Upload ID
      */
     String createUpload(UploadRequest upload, String healthCode);
 
+    /**
+     * Marks the specified upload as complete.
+     *
+     * @param uploadId
+     */
     void uploadComplete(String uploadId);
+
+    /**
+     * Is the upload complete?
+     */
+    boolean isComplete(String uploadId);
 }
