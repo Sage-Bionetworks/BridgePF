ad56d3183330fdbe2d0f994aa9b8720df294908b
Eric Wu <eric.wu@sagebase.org>
2014-10-09 19:47:37 -0700

    Clean up backfill.

--- app/org/sagebionetworks/bridge/BridgeConstants.java
+++ app/org/sagebionetworks/bridge/BridgeConstants.java
@@ -4,8 +4,6 @@ public class BridgeConstants {
 
     public static final String SESSION_TOKEN_HEADER = "Bridge-Session";
 
-    public static final String CUSTOM_DATA_CONSENT_SUFFIX = "_consent";
-
     public static final String CUSTOM_DATA_HEALTH_CODE_SUFFIX = "_code";
 
     public static final String CUSTOM_DATA_VERSION = "version";
