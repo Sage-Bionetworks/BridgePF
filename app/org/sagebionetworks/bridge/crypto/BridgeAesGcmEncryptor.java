3c796124035ac55e8cfb119d2509ffccc16c99be
Eric Wu <eric.wu@sagebase.org>
2014-10-24 13:21:48 -0700

    User the new AES encryptor for new accounts.

--- app/org/sagebionetworks/bridge/crypto/BridgeAesGcmEncryptor.java
+++ app/org/sagebionetworks/bridge/crypto/BridgeAesGcmEncryptor.java
@@ -13,6 +13,20 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class BridgeAesGcmEncryptor {
 
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            throw new RuntimeException("Must supply at least 2 parameters: "
+                    + "password, target, decrypt (optional, when missing, encrypt)");
+        }
+        final String password = args[0];
+        final BridgeAesGcmEncryptor encryptor = new BridgeAesGcmEncryptor(password);
+        if (args.length == 2 || "encrypt".equalsIgnoreCase(args[2])) {
+            System.out.println("Encrypted: " + encryptor.encrypt(args[1]));
+        } else {
+            System.out.println("Decrypted: " + encryptor.decrypt(args[1]));
+        }
+    }
+
     public BridgeAesGcmEncryptor(String password) {
 
         if (password == null || password.isEmpty()) {
