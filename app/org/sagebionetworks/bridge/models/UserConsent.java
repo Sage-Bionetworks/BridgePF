65472381bcfc8c6d8ff95a8bc48dec1a61c8bcec
Alx Dark <alx.dark@sagebase.org>
2014-10-31 15:59:31 -0700

    Adding signedOn as an available value

--- app/org/sagebionetworks/bridge/models/UserConsent.java
+++ app/org/sagebionetworks/bridge/models/UserConsent.java
@@ -2,10 +2,12 @@ package org.sagebionetworks.bridge.models;
 
 public interface UserConsent {
     
+    public long getSignedOn();
+
     public boolean getDataSharing();
-    
+
     public String getStudyKey();
-    
+
     public long getConsentCreatedOn();
 
 }
