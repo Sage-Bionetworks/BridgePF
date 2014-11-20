8931879a546cfaa9208d2f5a79625139fefd0f4e
Dwayne Jeng <dwayne.jeng@sagebase.org>
2014-11-20 12:26:58 -0800

    Consent Signature factory methods with validation (does not build)

--- app/org/sagebionetworks/bridge/models/studies/ConsentSignature.java
+++ app/org/sagebionetworks/bridge/models/studies/ConsentSignature.java
@@ -4,79 +4,77 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.common.base.Preconditions;
 
 import org.sagebionetworks.bridge.exceptions.InvalidEntityException;
 import org.sagebionetworks.bridge.json.JsonUtils;
 import org.sagebionetworks.bridge.models.BridgeEntity;
+import org.sagebionetworks.bridge.validators.ConsentSignatureValidator;
+import org.sagebionetworks.bridge.validators.Validate;
 
 public class ConsentSignature implements BridgeEntity {
 
     private static final String NAME_FIELD = "name";
     private static final String BIRTHDATE_FIELD = "birthdate";
-    private static final String IMAGE_FIELD = "image";
+    private static final String IMAGE_DATA_FIELD = "imageData";
+    private static final String IMAGE_MIME_TYPE = "imageMimeType";
+    private static final ConsentSignatureValidator VALIDATOR = new ConsentSignatureValidator();
 
     private final @Nonnull String name;
     private final @Nonnull String birthdate;
-    private final @Nullable ConsentSignatureImage image;
+    private final @Nullable String imageData;
+    private final @Nullable String imageMimeType;
+
+    /** Private constructor. Instances should be constructed using factory methods create() or createFromJson(). */
+    private ConsentSignature(String name, String birthdate, String imageData, String imageMimeType) {
+        this.name = name;
+        this.birthdate = birthdate;
+        this.imageData = imageData;
+        this.imageMimeType = imageMimeType;
+    }
 
     /**
      * <p>
-     * Simple constructor.
+     * Factory method to create and validate ConsentSignature.
      * </p>
      * <p>
-     * Signature image is nullable for backwards compatibility with the old format. ConsentController will ensure that
-     * all new signatures are created with a signature image. After the beta data is wiped, we can update this
-     * constructor to enforce non-null signature image.
+     * imageData and imageMimeType are optional. However, if one of them is specified, both of them must be specified.
+     * If they are specified, they must be non-empty.
      * </p>
      *
      * @param name
      *         name of the user giving consent, must be non-null and non-empty
      * @param birthdate
      *         user's birthday in the format "YYYY-MM-DD", must be non-null and non-empty
-     * @param image
-     *         signature image, may be null, must be non-empty
+     * @param imageData
+     *         image data as a Base64 encoded string, optional
+     * @param imageMimeType
+     *         image MIME type (ex: image/png), optioanl
+     * @return validated ConsentSignature
+     * @throws InvalidEntityException
+     *         if called with invalid fields
      */
-    public ConsentSignature(@Nonnull String name, @Nonnull String birthdate, @Nullable ConsentSignatureImage image) {
-        Preconditions.checkNotNull(name, "name must be non-null");
-        Preconditions.checkArgument(!name.isEmpty(), "name must be non-empty");
-        Preconditions.checkNotNull(birthdate, "birthdate must be non-null");
-        Preconditions.checkArgument(!birthdate.isEmpty(), "birthdate must be non-empty");
-
-        this.name = name;
-        this.birthdate = birthdate;
-        this.image = image;
+    public static ConsentSignature create(@Nonnull String name, @Nonnull String birthdate, @Nullable String imageData,
+            @Nullable String imageMimeType) throws InvalidEntityException {
+        ConsentSignature sig = new ConsentSignature(name, birthdate, imageData, imageMimeType);
+        Validate.entityThrowingException(VALIDATOR, sig);
+        return sig;
     }
 
     /**
-     * Construct from JSON. This method validates, but throws InvalidEntityException on invalid input. See constructor
-     * documentation for details
+     * Factory method to create and validate ConsentSignature from JSON. See {@link #create} for validation details.
      *
      * @param node
      *         JSON node to parse
-     * @return consent signature
+     * @return validated ConsentSignature
      * @throws InvalidEntityException
      *         if the JSON contains invalid fields
      */
-    public static ConsentSignature fromJson(JsonNode node) throws InvalidEntityException {
+    public static ConsentSignature createFromJson(JsonNode node) throws InvalidEntityException {
         String name = JsonUtils.asText(node, NAME_FIELD);
         String birthdate = JsonUtils.asText(node, BIRTHDATE_FIELD);
-        ObjectNode imageNode = JsonUtils.asObjectNode(node, IMAGE_FIELD);
-
-        if (name == null || name.isEmpty()) {
-            throw new InvalidEntityException("name must be specified");
-        }
-        if (birthdate == null || birthdate.isEmpty()) {
-            throw new InvalidEntityException("birthdate must be specified");
-        }
-
-        ConsentSignatureImage sigImg = null;
-        if (imageNode != null) {
-            sigImg = ConsentSignatureImage.fromJson(imageNode);
-        }
-
-        return new ConsentSignature(name, birthdate, sigImg);
+        String imageData = JsonUtils.asText(node, IMAGE_DATA_FIELD);
+        String imageMimeType = JsonUtils.asText(node, IMAGE_MIME_TYPE);
+        return create(name, birthdate, imageData, imageMimeType);
     }
 
     /** Name of the user giving consent. */
@@ -89,8 +87,13 @@ public class ConsentSignature implements BridgeEntity {
         return birthdate;
     }
 
-    /** Signature image. */
-    public @Nullable ConsentSignatureImage getImage() {
-        return image;
+    /** Image data as a Base64 encoded string. */
+    public @Nullable String getImageData() {
+        return imageData;
+    }
+
+    /** Image MIME type (ex: image/png). */
+    public @Nullable String getImageMimeType() {
+        return imageMimeType;
     }
 }
---
+++ app/org/sagebionetworks/bridge/validators/ConsentSignatureValidator.java
@@ -0,0 +1,39 @@
+package org.sagebionetworks.bridge.validators;
+
+import com.google.common.base.Strings;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import org.sagebionetworks.bridge.models.studies.ConsentSignature;
+
+public class ConsentSignatureValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return ConsentSignature.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        ConsentSignature sig = (ConsentSignature) target;
+        if (Strings.isNullOrEmpty(sig.getName())) {
+            errors.rejectValue("name", Validate.CANNOT_BE_BLANK);
+        }
+        if (Strings.isNullOrEmpty(sig.getBirthdate())) {
+            errors.rejectValue("birthdate", Validate.CANNOT_BE_BLANK);
+        }
+
+        String imageData = sig.getImageData();
+        String imageMimeType = sig.getImageMimeType();
+        if (imageData != null && imageData.isEmpty()) {
+            errors.rejectValue("imageData", Validate.CANNOT_BE_EMPTY_STRING);
+        }
+        if (imageMimeType != null && imageMimeType.isEmpty()) {
+            errors.rejectValue("imageMimeType", Validate.CANNOT_BE_EMPTY_STRING);
+        }
+
+        // if one of them is not null, but not both
+        if (imageData != null ^ imageMimeType != null) {
+            errors.reject("If you specify one of imageData or imageMimeType, you must specify both");
+        }
+    }
+}
