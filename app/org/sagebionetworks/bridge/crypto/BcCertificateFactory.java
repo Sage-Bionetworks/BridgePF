f0d746a1f53e3812d1bc017099e01b3fbb76f352
Eric Wu <eric.wu@sagebase.org>
2014-11-20 12:32:20 -0800

    Create cert with proper extensions

--- app/org/sagebionetworks/bridge/crypto/BcCertificateFactory.java
+++ app/org/sagebionetworks/bridge/crypto/BcCertificateFactory.java
@@ -9,10 +9,20 @@ import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.Date;
 
+import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x500.X500NameBuilder;
 import org.bouncycastle.asn1.x500.style.BCStyle;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.asn1.x509.BasicConstraints;
+import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.asn1.x509.GeneralName;
+import org.bouncycastle.asn1.x509.GeneralNames;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.cert.CertIOException;
 import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509ExtensionUtils;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.bc.BcX509ExtensionUtils;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
@@ -30,31 +40,48 @@ public class BcCertificateFactory implements CertificateFactory {
 
     @Override
     public X509Certificate newCertificate(KeyPair keyPair) {
+        CertificateInfo certInfo = new CertificateInfo()
+                .withCountry("US")
+                .withState("WA")
+                .withCity("Seattle")
+                .withOrganization("Sage Bionetworks")
+                .withTeam("Platform")
+                .withEmail("bridgeIT@sagebase.org")
+                .withFqdn("*.sagebridge.org");
+        return newCertificate(keyPair, certInfo);
+    }
 
-        final X500NameBuilder issuer = new X500NameBuilder(BCStyle.INSTANCE);
-        // Common name
-        issuer.addRDN(BCStyle.CN, "Sage Bridge Self-Signed");
-        // Organizational unit name
-        issuer.addRDN(BCStyle.OU, "Bridge Project");
-        // Organization name
-        issuer.addRDN(BCStyle.O, "Sage Bionetworks");
-        // Country code
-        issuer.addRDN(BCStyle.C, "US");
+    @Override
+    public X509Certificate newCertificate(KeyPair keyPair, CertificateInfo certInfo) {
 
-        final X500NameBuilder subject = new X500NameBuilder(BCStyle.INSTANCE);
-        subject.addRDN(BCStyle.CN, "Sage Bridge Self-Signed");
-
-        // TODO: Serial
-        BigInteger serial = BigInteger.ONE;
-
+        // Create a cert builder
         DateTime now = DateTime.now(DateTimeZone.UTC);
-        Date notBefore = now.minusDays(1).toDate();
-        Date notAfter = now.plusDays(365).toDate();
+        final Date notBefore = now.minusDays(2).toDate();
+        final Date notAfter = now.plusDays(9999).toDate();
+        final X500Name issuer = getIssuer(certInfo);
+        final X500Name subject = getSubject(certInfo); 
+        final BigInteger serial = new BigInteger(Long.toString(System.nanoTime()));
+        final PublicKey publicKey = keyPair.getPublic();
+        final X509v3CertificateBuilder certBuilder = new JcaX509v3CertificateBuilder(
+                issuer, serial, notBefore, notAfter, subject, publicKey);
 
-        X509v3CertificateBuilder certBuilder = new JcaX509v3CertificateBuilder(
-                issuer.build(), serial, notBefore, notAfter, subject.build(), keyPair.getPublic());
-        // TODO: Add extensions here to the cert builder
+        // Add extensions
+        try {
+            // Certificate Authority: YES
+            certBuilder.addExtension(Extension.basicConstraints, false, new BasicConstraints(true));
+            final X509ExtensionUtils extUtils = new BcX509ExtensionUtils();
+            final SubjectPublicKeyInfo publicKeyInfo = new SubjectPublicKeyInfo(
+                    AlgorithmIdentifier.getInstance(BcCmsConstants.KEY_PAIR_ALGO_ID), publicKey.getEncoded());
+            GeneralNames names = new GeneralNames(new GeneralName(issuer));
+            certBuilder.addExtension(Extension.authorityKeyIdentifier, false,
+                    extUtils.createAuthorityKeyIdentifier(publicKeyInfo, names, serial));
+            certBuilder.addExtension(Extension.subjectKeyIdentifier, false,
+                    extUtils.createSubjectKeyIdentifier(publicKeyInfo));
+        } catch (CertIOException e) {
+            throw new RuntimeException(e);
+        }
 
+        // Build a cert
         try {
             JcaContentSignerBuilder signerBuilder = new JcaContentSignerBuilder(BcCmsConstants.SIGNER_ALGO).setProvider(BcCmsConstants.PROVIDER);
             ContentSigner consentSigner = signerBuilder.build(keyPair.getPrivate());
@@ -77,7 +104,6 @@ public class BcCertificateFactory implements CertificateFactory {
         final X500NameBuilder subject = (new X500NameBuilder(BCStyle.INSTANCE)).addRDN(BCStyle.CN, "Sage Bridge Self-Signed");
         X509v3CertificateBuilder certBuilder = new JcaX509v3CertificateBuilder(
                 caCert, serial, notBefore, notAfter, subject.build(), publicKey);
-        // TODO: Add extensions here to the builder
 
         try {
             JcaContentSignerBuilder signerBuilder = new JcaContentSignerBuilder(BcCmsConstants.SIGNER_ALGO).setProvider(BcCmsConstants.PROVIDER);
@@ -89,5 +115,29 @@ public class BcCertificateFactory implements CertificateFactory {
         } catch (CertificateException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private X500Name getSubject(CertificateInfo certInfo) {
+        X500NameBuilder issuer = new X500NameBuilder(BCStyle.INSTANCE);
+        issuer.addRDN(BCStyle.C, certInfo.getCountry());
+        issuer.addRDN(BCStyle.ST, certInfo.getState());
+        issuer.addRDN(BCStyle.L, certInfo.getCity());
+        issuer.addRDN(BCStyle.O, certInfo.getOrganization());
+        issuer.addRDN(BCStyle.OU, certInfo.getTeam());
+        issuer.addRDN(BCStyle.EmailAddress, certInfo.getEmail());
+        issuer.addRDN(BCStyle.CN, certInfo.getFqdn());
+        return issuer.build();
+    }
+
+    private X500Name getIssuer(CertificateInfo certInfo) {
+        X500NameBuilder issuer = new X500NameBuilder(BCStyle.INSTANCE);
+        issuer.addRDN(BCStyle.C, certInfo.getCountry());
+        issuer.addRDN(BCStyle.ST, certInfo.getState());
+        issuer.addRDN(BCStyle.L, certInfo.getCity());
+        issuer.addRDN(BCStyle.O, certInfo.getOrganization());
+        issuer.addRDN(BCStyle.OU, certInfo.getTeam());
+        issuer.addRDN(BCStyle.EmailAddress, certInfo.getEmail());
+        issuer.addRDN(BCStyle.CN, certInfo.getFqdn());
+        return issuer.build();
     }
 }
