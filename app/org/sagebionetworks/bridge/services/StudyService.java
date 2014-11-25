4ea1bce347d7922df451e386564a76c26405b46d
Eric Wu <eric.wu@sagebase.org>
2014-11-25 15:19:52 -0800

    Remove whitespace

--- app/org/sagebionetworks/bridge/services/StudyService.java
+++ app/org/sagebionetworks/bridge/services/StudyService.java
@@ -6,19 +6,18 @@ import org.sagebionetworks.bridge.models.studies.Study;
 import org.sagebionetworks.bridge.models.studies.Tracker;
 
 public interface StudyService {
-    
+
     public Tracker getTrackerByIdentifier(String trackerId);
-    
+
     public Study getStudyByIdentifier(String identifier);
-    
+
     public Study getStudyByHostname(String hostname);
-    
+
     public List<Study> getStudies();
-    
+
     public Study createStudy(Study study);
-    
+
     public Study updateStudy(Study study);
-    
+
     public void deleteStudy(String identifier);
-    
 }
