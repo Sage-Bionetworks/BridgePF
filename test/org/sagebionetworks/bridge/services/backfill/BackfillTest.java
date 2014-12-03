6f4755553492f6cee1870142e770e9ce92c4f6fe
Eric Wu <eric.wu@sagebase.org>
2014-12-02 22:14:04 -0800

    Add skeleton backfill test

--- test/org/sagebionetworks/bridge/services/backfill/BackfillTest.java
+++ test/org/sagebionetworks/bridge/services/backfill/BackfillTest.java
@@ -1,20 +1,13 @@
 package org.sagebionetworks.bridge.services.backfill;
 
-import javax.annotation.Resource;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.amazonaws.services.s3.AmazonS3;
-
 @ContextConfiguration("classpath:test-context.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
-public class StudyCertificateBackfillTest {
-
-    @Resource
-    private AmazonS3 s3Client;
+public class BackfillTest {
 
     @Test
     public void test() {
