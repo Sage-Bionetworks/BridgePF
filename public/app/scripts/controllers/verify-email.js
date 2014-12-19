edf2ca3a7fee40900d3f2d9ecf8e832c4546a6e0
alxdarksage <alx.dark@sagebase.org>
2014-12-19 15:59:15 -0800

    Creating a simple email verification page in the app; there's now no ability to reset your password through the app, or anything else

--- public/app/scripts/controllers/verify-email.js
+++ public/app/scripts/controllers/verify-email.js
@@ -3,18 +3,22 @@ function($scope, $route, authService, formService) {
     
     formService.initScope($scope, 'noForm');
     $scope.setMessage("Verifying...");
-
+    $scope.succeeded = false;
     $scope.sptoken = formService.retrieveSpToken($route);
     
     authService.verifyEmail({sptoken: $scope.sptoken}).then(function(response) {
-        $scope.setMessage("Your email address has been verified.");
+        $scope.setMessage("");
+        $scope.succeeded = 'loaded';
     }, function(response) {
         if (response.status === 412) {
             // authService.initSession(response.data);
             // Just exactly as if nothing had happened.
             $scope.setMessage("Your email address has been verified.");
         } else {
-            $scope.setMessage(response.data.message, "danger");    
+            if (response.data.message === "The requested resource does not exist.") {
+                response.data.message = "Your email address has already been verified.";
+            }
+            $scope.setMessage(response.data.message, "danger");
         }
     });
     
