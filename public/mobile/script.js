61d3183c19dbbfeec0280fd633bf87c2b225c7b2
alxdarksage <alx.dark@sagebase.org>
2015-01-08 13:56:49 -0800

    Didn't account for -develop, -staging, etc

--- public/mobile/script.js
+++ public/mobile/script.js
@@ -10,8 +10,7 @@ var titles = {
 var qd = {};
 location.search.substr(1).split("&").forEach(function(item) {var k = item.split("=")[0], v = decodeURIComponent(item.split("=")[1]); (k in qd) ? qd[k].push(v) : qd[k] = [v,]});
 
-var host = document.location.hostname;
-host = host.split(".")[0];
+var host = document.location.host.match(/^(.*?)\W/)[1];
 if (!titles[host]) {
     host = "api";
 }
