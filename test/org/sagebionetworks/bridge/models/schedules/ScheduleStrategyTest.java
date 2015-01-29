9242d640db597c28596e3ff3a94d1da6e823c946
alxdarksage <alx.dark@sagebase.org>
2015-01-29 13:10:57 -0800

    Remove unused import

--- test/org/sagebionetworks/bridge/models/schedules/ScheduleStrategyTest.java
+++ test/org/sagebionetworks/bridge/models/schedules/ScheduleStrategyTest.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
