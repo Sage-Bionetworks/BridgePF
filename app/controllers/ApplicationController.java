5ffe85e86d3db551f96d70c5a0106ad9fab457f2
Eric Wu <eric.wu@sagebase.org>
2015-02-04 14:48:55 -0800

    Clean up

--- app/controllers/ApplicationController.java
+++ app/controllers/ApplicationController.java
@@ -27,38 +27,30 @@ public class ApplicationController extends BaseController {
 
     public Result loadPublicApp() throws Exception {
         UserSessionInfo info = new UserSessionInfo(new UserSession());
-        
-        // We need to default to a study or the integration tests in Play will fail 
-        // (localhost:3333 won't match anything).
         try {
             Study study = studyService.getStudyByHostname(getHostname());
             if ("pd".equals(study.getIdentifier()) || "neurod".equals(study.getIdentifier()) || "parkinson".equals(study.getIdentifier())) {
-                return ok(views.html.neurod.render(Json.toJson(info).toString(), ASSETS_HOST, ASSETS_BUILD));    
+                return ok(views.html.neurod.render(Json.toJson(info).toString(), ASSETS_HOST, ASSETS_BUILD));
             } else if ("api".equals(study.getIdentifier())) {
                 return ok(views.html.api.render(Json.toJson(info).toString(), ASSETS_HOST, ASSETS_BUILD));
             }
             String apiHost = "api" + BridgeConfigFactory.getConfig().getStudyHostnamePostfix();
             return ok(views.html.nosite.render(study.getName(), apiHost));
         } catch(EntityNotFoundException e) {
-            // Go with the API study
             return ok(views.html.api.render(Json.toJson(info).toString(), ASSETS_HOST, ASSETS_BUILD));
         }
     }
-    
+
     public Result loadConsent(String sessionToken) throws Exception {
         UserSession session = null;
         if (sessionToken != null) {
-            session = cacheProvider.getUserSession(sessionToken);    
+            session = cacheProvider.getUserSession(sessionToken);
         }
         if (session == null) {
             session = EMPTY_USER_SESSION;
         }
         UserSessionInfo info = new UserSessionInfo(session);
         return ok(views.html.consent.render(Json.toJson(info).toString(), ASSETS_HOST, ASSETS_BUILD));
-    }
-
-    public Result redirectToConsent() {
-        return redirect("/consent/");
     }
 
     public Result preflight(String all) {
