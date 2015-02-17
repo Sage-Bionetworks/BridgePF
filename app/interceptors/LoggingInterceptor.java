9a73291c0b57ffadcc13f79fcd36a597606d4a27
alxdarksage <alx.dark@sagebase.org>
2015-02-17 15:56:47 -0800

    Removing thread from loggers (we have request-id on Heroku for now); logging more standard access log than UA

--- app/interceptors/LoggingInterceptor.java
+++ app/interceptors/LoggingInterceptor.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import play.mvc.Http;
+import play.mvc.Result;
 
 public class LoggingInterceptor implements MethodInterceptor {
 
@@ -13,13 +14,23 @@ public class LoggingInterceptor implements MethodInterceptor {
 
     @Override
     public Object invoke(MethodInvocation method) throws Throwable {
+        Result result = (Result)method.proceed();
         if (logger.isInfoEnabled()) {
+            // Common Log Format for access logging, minus the first part which 
+            // is already part of our standard logging output.
             Http.Request request = Http.Context.current().request();
+            String address = request.remoteAddress();
+            String path = request.path();
+            String verb = request.method();
+            String version = request.version();
             String[] values = request.headers().get("User-Agent");
-            if (values != null && values.length > 0) {
-                logger.info("User-Agent: " + values[0]);
-            }
+            String userAgent = (values != null && values.length > 0) ? values[0] : "";
+            play.mvc.Results.Status statusObj = (play.mvc.Results.Status)result;
+            int status = statusObj.getWrappedSimpleResult().header().status();
+            
+            String output = String.format("%s \"%s %s %s\" %s %s", address, verb, path, version, status, userAgent);
+            logger.info(output);
         }
-        return method.proceed();
+        return result;
     }
 }
