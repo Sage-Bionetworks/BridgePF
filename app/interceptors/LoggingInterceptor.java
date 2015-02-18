3cd079baf8c14b6ad578d3e04468f770f12a4c5c
alxdarksage <alx.dark@sagebase.org>
2015-02-18 11:15:29 -0800

    Updated based on review

--- app/interceptors/LoggingInterceptor.java
+++ app/interceptors/LoggingInterceptor.java
@@ -19,18 +19,25 @@ public class LoggingInterceptor implements MethodInterceptor {
             // Common Log Format for access logging, minus the first part which 
             // is already part of our standard logging output.
             Http.Request request = Http.Context.current().request();
-            String address = request.remoteAddress();
+            
             String path = request.path();
             String verb = request.method();
             String version = request.version();
-            String[] values = request.headers().get("User-Agent");
-            String userAgent = (values != null && values.length > 0) ? values[0] : "";
+            String address = header(request, "X-Forwarded-For", request.remoteAddress());
+            String requestId = header(request, "X-Request-Id", "-");
+            String userAgent = header(request, "User-Agent", "-");
+            
             play.mvc.Results.Status statusObj = (play.mvc.Results.Status)result;
             int status = statusObj.getWrappedSimpleResult().header().status();
             
-            String output = String.format("%s \"%s %s %s\" %s %s", address, verb, path, version, status, userAgent);
+            String output = String.format("%s [%s] \"%s %s %s\" %s %s", address, requestId, verb, path, version, status, userAgent);
             logger.info(output);
         }
         return result;
+    }
+    
+    private String header(Http.Request request, String name, String defaultValue) {
+        String[] values = request.headers().get(name);
+        return (values != null && values.length > 0) ? values[0] : defaultValue;
     }
 }
