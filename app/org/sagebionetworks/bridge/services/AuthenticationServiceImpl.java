aec7f154d09cb0aa4c833777b0160809c5596105
alxdarksage <alx.dark@sagebase.org>
2015-03-02 12:51:09 -0800

    Autoproxying added.

--- app/org/sagebionetworks/bridge/services/AuthenticationServiceImpl.java
+++ app/org/sagebionetworks/bridge/services/AuthenticationServiceImpl.java
@@ -35,7 +35,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("authenticationService")
 public class AuthenticationServiceImpl implements AuthenticationService {
     
     private final Logger logger = LoggerFactory.getLogger(AuthenticationServiceImpl.class);
@@ -105,17 +105,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         checkNotNull(study, "Study cannot be null");
         checkNotNull(signIn, "Sign in cannot be null");
 
-        checkNotNull(lockDao);
-        checkNotNull(cacheProvider);
-        checkNotNull(config);
-        checkNotNull(consentService);
-        checkNotNull(optionsService);
-        checkNotNull(accountDao);
-        checkNotNull(healthCodeService);
-        checkNotNull(signInValidator);
-        checkNotNull(signUpValidator);
-        checkNotNull(passwordResetValidator);        
-        
         Validate.entityThrowingException(signInValidator, signIn);
 
         Account account = accountDao.authenticate(study, signIn);
