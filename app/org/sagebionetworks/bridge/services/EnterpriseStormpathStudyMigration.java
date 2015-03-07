542741a4f25ad6df2c3eab61236b2904a1e42cb2
alxdarksage <alx.dark@sagebase.org>
2015-03-06 19:48:28 -0800

    Removing logging; disabling the migrator (everything migrated)

--- app/org/sagebionetworks/bridge/services/EnterpriseStormpathStudyMigration.java
+++ app/org/sagebionetworks/bridge/services/EnterpriseStormpathStudyMigration.java
@@ -117,6 +117,7 @@ public class EnterpriseStormpathStudyMigration implements ApplicationListener<Co
     @Override
     public void onApplicationEvent(final ContextRefreshedEvent event) {
         // Switch to backMap to undo this migration.
+        /*
         Map<Environment, Map<String, String>> targetMap = foreMap;
         
         Environment env = BridgeConfigFactory.getConfig().getEnvironment();
@@ -142,6 +143,7 @@ public class EnterpriseStormpathStudyMigration implements ApplicationListener<Co
                 e.printStackTrace();
             }
         }
+        */
     }
     
     private void log(String msg, String studyId, String enterpriseHref) {
