6bd2b55e3b34ff524782cc902f9619395b5e3d15
alxdarksage <alx.dark@sagebase.org>
2015-03-13 10:30:33 -0700

    Removing unused web pages/routes

--- app/controllers/ApplicationController.java
+++ app/controllers/ApplicationController.java
@@ -2,8 +2,6 @@ package controllers;
 
 import static org.sagebionetworks.bridge.BridgeConstants.ASSETS_HOST;
 
-import org.sagebionetworks.bridge.config.BridgeConfigFactory;
-import org.sagebionetworks.bridge.exceptions.EntityNotFoundException;
 import org.sagebionetworks.bridge.models.UserSession;
 import org.sagebionetworks.bridge.models.UserSessionInfo;
 import org.springframework.stereotype.Controller;
@@ -14,44 +12,19 @@ import play.mvc.Result;
 @Controller("applicationController")
 public class ApplicationController extends BaseController {
 
-    private static final UserSession EMPTY_USER_SESSION = new UserSession();
+    private static final UserSessionInfo EMPTY_USER_SESSION = new UserSessionInfo(new UserSession());
     private static final String ASSETS_BUILD = "201501291830";
 
-    public Result redirectToPublicApp() {
-        return redirect("/");
-    }
-
     public Result loadApp() throws Exception {
-        return loadPublicApp();
+        return ok(views.html.index.render());
     }
 
-    public Result loadPublicApp() throws Exception {
-        UserSessionInfo info = new UserSessionInfo(new UserSession());
-        try {
-            // Study study = getStudy(); don't need the entire study, so don't get it
-            String studyIdentifier = getStudyIdentifier();
-            if ("parkinson".equals(studyIdentifier)) {
-                return ok(views.html.neurod.render(Json.toJson(info).toString(), ASSETS_HOST, ASSETS_BUILD));
-            } else if ("api".equals(studyIdentifier)) {
-                return ok(views.html.api.render(Json.toJson(info).toString(), ASSETS_HOST, ASSETS_BUILD));
-            }
-            String apiHost = "api" + BridgeConfigFactory.getConfig().getStudyHostnamePostfix();
-            return ok(views.html.nosite.render("Unknown study", apiHost));
-        } catch(EntityNotFoundException e) {
-            return ok(views.html.api.render(Json.toJson(info).toString(), ASSETS_HOST, ASSETS_BUILD));
-        }
+    public Result verifyEmail() {
+        return ok(views.html.verifyEmail.render(Json.toJson(EMPTY_USER_SESSION).toString(), ASSETS_HOST, ASSETS_BUILD));
     }
 
-    public Result loadConsent(String sessionToken) throws Exception {
-        UserSession session = null;
-        if (sessionToken != null) {
-            session = cacheProvider.getUserSession(sessionToken);
-        }
-        if (session == null) {
-            session = EMPTY_USER_SESSION;
-        }
-        UserSessionInfo info = new UserSessionInfo(session);
-        return ok(views.html.consent.render(Json.toJson(info).toString(), ASSETS_HOST, ASSETS_BUILD));
+    public Result resetPassword() {
+        return ok(views.html.resetPassword.render(Json.toJson(EMPTY_USER_SESSION).toString(), ASSETS_HOST, ASSETS_BUILD));
     }
 
     public Result preflight(String all) {
@@ -59,15 +32,5 @@ public class ApplicationController extends BaseController {
         response().setHeader(ACCESS_CONTROL_ALLOW_METHODS, "HEAD, GET, OPTIONS, POST, PUT, DELETE");
         response().setHeader(ACCESS_CONTROL_ALLOW_HEADERS, "*");
         return ok();
-    }
-
-    public Result verifyEmail() {
-        UserSessionInfo info = new UserSessionInfo(new UserSession());
-        return ok(views.html.verifyEmail.render(Json.toJson(info).toString(), ASSETS_HOST, ASSETS_BUILD));
-    }
-
-    public Result resetPassword() {
-        UserSessionInfo info = new UserSessionInfo(new UserSession());
-        return ok(views.html.resetPassword.render(Json.toJson(info).toString(), ASSETS_HOST, ASSETS_BUILD));
     }
 }
