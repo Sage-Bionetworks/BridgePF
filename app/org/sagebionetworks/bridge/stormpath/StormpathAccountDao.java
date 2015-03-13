243f4908ed92a90ea03fcf931a852b15655350b1
Eric Wu <eric.wu@sagebase.org>
2015-03-13 14:20:34 -0700

    Remove printlines

--- app/org/sagebionetworks/bridge/stormpath/StormpathAccountDao.java
+++ app/org/sagebionetworks/bridge/stormpath/StormpathAccountDao.java
@@ -218,9 +218,6 @@ public class StormpathAccountDao implements AccountDao {
         checkArgument(isNotBlank(signIn.getPassword()));
         
         try {
-            System.out.println(" XXX " + study.getIdentifier());
-            System.out.println(" XXX " + study.getName());
-            System.out.println(" XXX " + study.getStormpathHref());
             Directory directory = client.getResource(study.getStormpathHref(), Directory.class);
             
             UsernamePasswordRequest request = new UsernamePasswordRequest(signIn.getUsername(), signIn.getPassword(), directory);
