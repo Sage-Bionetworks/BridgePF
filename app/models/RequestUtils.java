d22e8285c8a52488537d769e99e617114e10aa51
Eric Wu <eric.wu@sagebase.org>
2015-03-19 23:49:01 -0700

    Request id

--- app/models/RequestUtils.java
+++ app/models/RequestUtils.java
@@ -1,9 +1,6 @@
 package models;
 
 import static org.sagebionetworks.bridge.BridgeConstants.X_REQUEST_ID_HEADER;
-
-import java.util.UUID;
-
 import play.mvc.Http.Request;
 
 public final class RequestUtils {
@@ -12,7 +9,8 @@ public final class RequestUtils {
     }
 
     public static String getRequestId(final Request request) {
-        return header(request, X_REQUEST_ID_HEADER, UUID.randomUUID().toString());
+        return header(request, X_REQUEST_ID_HEADER,
+                Integer.toHexString(System.identityHashCode(request)));
     }
 
     public static String header(final Request request, final String name, final String defaultVal) {
