9cf52e58b6a65a7768b8487ee40feaed9a651af4
eric.wu <eric.wu@sagebase.org>
2015-05-20 14:10:39 -0700

    Add JedisTransaction

---
+++ app/org/sagebionetworks/bridge/redis/JedisTransaction.java
@@ -0,0 +1,52 @@
+package org.sagebionetworks.bridge.redis;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
+import redis.clients.jedis.exceptions.JedisException;
+
+/**
+ * Provides abstraction over Jedis's transactions.
+ */
+final class JedisTransaction {
+
+    private final Logger logger = LoggerFactory.getLogger(JedisTransaction.class);
+
+    private final Jedis jedis;
+    private final Transaction transaction;
+
+    JedisTransaction(Jedis jedis) {
+        this.jedis = jedis;
+        this.transaction = jedis.multi();
+    }
+
+    public JedisTransaction setex(final String key, final int seconds, final String value) {
+        transaction.setex(key, seconds, value);
+        return this;
+    }
+
+    public List<Object> exec() {
+        try (Jedis jedis = this.jedis) {
+            return transaction.exec();
+        }
+    }
+
+    public String discard() {
+        try (Jedis jedis = this.jedis) {
+            return transaction.discard();
+        }
+    }
+
+    @Override
+    public void finalize() {
+        try {
+            jedis.close();
+        } catch (JedisException e) {
+            logger.debug(e.getMessage(), e);
+        }
+    }
+}
