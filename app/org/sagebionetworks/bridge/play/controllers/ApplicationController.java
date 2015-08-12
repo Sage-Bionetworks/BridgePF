6dfba01a1e9d4a26361a58b1026c823689b1659a
alxdarksage <alx.dark@sagebase.org>
2015-08-12 09:58:42 -0700

    Switching to use of study for email and name

--- app/org/sagebionetworks/bridge/play/controllers/ApplicationController.java
+++ app/org/sagebionetworks/bridge/play/controllers/ApplicationController.java
@@ -4,6 +4,7 @@ import static org.sagebionetworks.bridge.BridgeConstants.ASSETS_HOST;
 
 import org.sagebionetworks.bridge.models.accounts.UserSession;
 import org.sagebionetworks.bridge.models.accounts.UserSessionInfo;
+import org.sagebionetworks.bridge.models.studies.Study;
 import org.springframework.stereotype.Controller;
 
 import play.libs.Json;
@@ -12,18 +13,19 @@ import play.mvc.Result;
 @Controller
 public class ApplicationController extends BaseController {
 
-    private static final UserSessionInfo EMPTY_USER_SESSION = new UserSessionInfo(new UserSession());
     private static final String ASSETS_BUILD = "201501291830";
 
     public Result loadApp() throws Exception {
         return ok(views.html.index.render());
     }
 
-    public Result verifyEmail() {
-        return ok(views.html.verifyEmail.render(Json.toJson(EMPTY_USER_SESSION).toString(), ASSETS_HOST, ASSETS_BUILD));
+    public Result verifyEmail(String study) {
+        Study studyObj = studyService.getStudy(study);
+        return ok(views.html.verifyEmail.render(ASSETS_HOST, ASSETS_BUILD, studyObj.getName(), studyObj.getSupportEmail()));
     }
 
-    public Result resetPassword() {
-        return ok(views.html.resetPassword.render(Json.toJson(EMPTY_USER_SESSION).toString(), ASSETS_HOST, ASSETS_BUILD));
+    public Result resetPassword(String study) {
+        Study studyObj = studyService.getStudy(study);
+        return ok(views.html.resetPassword.render(ASSETS_HOST, ASSETS_BUILD, studyObj.getName(), studyObj.getSupportEmail()));
     }
 }
