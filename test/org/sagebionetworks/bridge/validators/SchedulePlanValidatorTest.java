079b602b03b368f321397233bab21f2929906ff1
alxdarksage <alx.dark@sagebase.org>
2015-10-08 16:20:24 -0700

    Fixing tabs

--- test/org/sagebionetworks/bridge/validators/SchedulePlanValidatorTest.java
+++ test/org/sagebionetworks/bridge/validators/SchedulePlanValidatorTest.java
@@ -15,39 +15,39 @@ import org.sagebionetworks.bridge.models.schedules.SimpleScheduleStrategy;
 
 public class SchedulePlanValidatorTest {
 
-	private SchedulePlanValidator validator;
+    private SchedulePlanValidator validator;
 
-	@Before
-	public void before() throws Exception {
-		validator = new SchedulePlanValidator();
-	}
+    @Before
+    public void before() throws Exception {
+        validator = new SchedulePlanValidator();
+    }
 
-	@Test
-	public void validSchedulePlan() {
-		SchedulePlan plan = getValidSimpleStrategy();
-		Validate.entityThrowingException(validator, plan);
+    @Test
+    public void validSchedulePlan() {
+        SchedulePlan plan = getValidSimpleStrategy();
+        Validate.entityThrowingException(validator, plan);
 
-		plan = getValidABTestStrategy();
-		Validate.entityThrowingException(validator, plan);
-	}
-	
-	@Test
-	public void itIsValidToSetMinMaxToSameVersion() {
+        plan = getValidABTestStrategy();
+        Validate.entityThrowingException(validator, plan);
+    }
+    
+    @Test
+    public void itIsValidToSetMinMaxToSameVersion() {
         SchedulePlan plan = getValidSimpleStrategy();
         plan.setMinAppVersion(1);
         plan.setMaxAppVersion(1);
         Validate.entityThrowingException(validator, plan);
-	}
+    }
 
-	@Test
-	public void cannotSetMaxUnderMinAppVersion() {
-	    SchedulePlan plan = getValidSimpleStrategy();
-	    plan.setMinAppVersion(0);
-	    plan.setMaxAppVersion(-1);
-	    assertMessage(plan, "maxAppVersion cannot be less than minAppVersion", "maxAppVersion");
-	}
-	
-	@Test
+    @Test
+    public void cannotSetMaxUnderMinAppVersion() {
+        SchedulePlan plan = getValidSimpleStrategy();
+        plan.setMinAppVersion(0);
+        plan.setMaxAppVersion(-1);
+        assertMessage(plan, "maxAppVersion cannot be less than minAppVersion", "maxAppVersion");
+    }
+    
+    @Test
     public void cannotSetMinLessThanZero() {
         SchedulePlan plan = getValidSimpleStrategy();
         plan.setMinAppVersion(-2);
@@ -55,91 +55,90 @@ public class SchedulePlanValidatorTest {
         assertMessage(plan, "minAppVersion cannot be negative", "minAppVersion");
         assertMessage(plan, "maxAppVersion cannot be negative", "maxAppVersion");
     }
-	
-	
-	@Test
-	public void studyKeyRequired() {
-		SchedulePlan plan = getValidSimpleStrategy();
-		plan.setStudyKey(null);
-		assertMessage(plan, "studyKey cannot be missing, null, or blank", "studyKey");
-	}
+    
+    @Test
+    public void studyKeyRequired() {
+        SchedulePlan plan = getValidSimpleStrategy();
+        plan.setStudyKey(null);
+        assertMessage(plan, "studyKey cannot be missing, null, or blank", "studyKey");
+    }
 
-	@Test
-	public void labelRequired() {
-		SchedulePlan plan = getValidSimpleStrategy();
-		plan.setLabel(null);
-		assertMessage(plan, "label cannot be missing, null, or blank", "label");
-	}
+    @Test
+    public void labelRequired() {
+        SchedulePlan plan = getValidSimpleStrategy();
+        plan.setLabel(null);
+        assertMessage(plan, "label cannot be missing, null, or blank", "label");
+    }
 
-	@Test
-	public void strategyRequired() {
-		SchedulePlan plan = getValidSimpleStrategy();
-		plan.setStrategy(null);
-		assertMessage(plan, "strategy is required", "strategy");
-	}
+    @Test
+    public void strategyRequired() {
+        SchedulePlan plan = getValidSimpleStrategy();
+        plan.setStrategy(null);
+        assertMessage(plan, "strategy is required", "strategy");
+    }
 
-	@Test
-	public void simpleStrategyScheduleRequired() {
-		SchedulePlan plan = getValidSimpleStrategy();
-		((SimpleScheduleStrategy)plan.getStrategy()).setSchedule(null);
-		assertMessage(plan, "strategy.schedule is required", "strategy.schedule");
-	}
-	
-	@Test
-	public void abTestStrategyScheduleAddsUpTo100() {
-		SchedulePlan plan = getValidABTestStrategy();
-		((ABTestScheduleStrategy)plan.getStrategy()).getScheduleGroups().get(0).setPercentage(10);
-		
-		assertMessage(plan, "strategy.scheduleGroups groups must add up to 100% (not 40%; give 20% as 20, for example)", "strategy.scheduleGroups");
-	}
-	
-	@Test
-	public void abTestStrategyScheduleRequiresSchedule() {
-		SchedulePlan plan = getValidABTestStrategy();
-		((ABTestScheduleStrategy)plan.getStrategy()).getScheduleGroups().get(0).setSchedule(null);
-		
-		assertMessage(plan, "strategy.scheduleGroups0.schedule is required", "strategy.scheduleGroups0.schedule");
-	}
+    @Test
+    public void simpleStrategyScheduleRequired() {
+        SchedulePlan plan = getValidSimpleStrategy();
+        ((SimpleScheduleStrategy)plan.getStrategy()).setSchedule(null);
+        assertMessage(plan, "strategy.schedule is required", "strategy.schedule");
+    }
+    
+    @Test
+    public void abTestStrategyScheduleAddsUpTo100() {
+        SchedulePlan plan = getValidABTestStrategy();
+        ((ABTestScheduleStrategy)plan.getStrategy()).getScheduleGroups().get(0).setPercentage(10);
+        
+        assertMessage(plan, "strategy.scheduleGroups groups must add up to 100% (not 40%; give 20% as 20, for example)", "strategy.scheduleGroups");
+    }
+    
+    @Test
+    public void abTestStrategyScheduleRequiresSchedule() {
+        SchedulePlan plan = getValidABTestStrategy();
+        ((ABTestScheduleStrategy)plan.getStrategy()).getScheduleGroups().get(0).setSchedule(null);
+        
+        assertMessage(plan, "strategy.scheduleGroups0.schedule is required", "strategy.scheduleGroups0.schedule");
+    }
 
-	private void assertMessage(SchedulePlan plan, String message, String fieldName) {
-		try {
-			Validate.entityThrowingException(validator, plan);
-			fail("Should have thrown exception");
-		} catch (InvalidEntityException e) {
-			assertEquals(message, e.getErrors().get(fieldName).get(0));
-		}
-	}
+    private void assertMessage(SchedulePlan plan, String message, String fieldName) {
+        try {
+            Validate.entityThrowingException(validator, plan);
+            fail("Should have thrown exception");
+        } catch (InvalidEntityException e) {
+            assertEquals(message, e.getErrors().get(fieldName).get(0));
+        }
+    }
 
-	private Schedule getSchedule() {
-		Schedule schedule = new Schedule();
-		schedule.setScheduleType(ScheduleType.ONCE);
-		schedule.setLabel("a label");
-		schedule.addActivity(TestConstants.TEST_3_ACTIVITY);
-		return schedule;
-	}
+    private Schedule getSchedule() {
+        Schedule schedule = new Schedule();
+        schedule.setScheduleType(ScheduleType.ONCE);
+        schedule.setLabel("a label");
+        schedule.addActivity(TestConstants.TEST_3_ACTIVITY);
+        return schedule;
+    }
 
-	private SchedulePlan getValidSimpleStrategy() {
-		SimpleScheduleStrategy strategy = new SimpleScheduleStrategy();
-		strategy.setSchedule(getSchedule());
+    private SchedulePlan getValidSimpleStrategy() {
+        SimpleScheduleStrategy strategy = new SimpleScheduleStrategy();
+        strategy.setSchedule(getSchedule());
 
-		// This is valid, then you can manipulate it to invalidate it.
-		SchedulePlan plan = new DynamoSchedulePlan();
-		plan.setLabel("a label");
-		plan.setStudyKey("study-key");
-		plan.setStrategy(strategy);
-		return plan;
-	}
+        // This is valid, then you can manipulate it to invalidate it.
+        SchedulePlan plan = new DynamoSchedulePlan();
+        plan.setLabel("a label");
+        plan.setStudyKey("study-key");
+        plan.setStrategy(strategy);
+        return plan;
+    }
 
-	private SchedulePlan getValidABTestStrategy() {
-		ABTestScheduleStrategy strategy = new ABTestScheduleStrategy();
-		strategy.addGroup(70, getSchedule());
-		strategy.addGroup(30, getSchedule());
+    private SchedulePlan getValidABTestStrategy() {
+        ABTestScheduleStrategy strategy = new ABTestScheduleStrategy();
+        strategy.addGroup(70, getSchedule());
+        strategy.addGroup(30, getSchedule());
 
-		// This is valid, then you can manipulate it to invalidate it.
-		SchedulePlan plan = new DynamoSchedulePlan();
-		plan.setLabel("a label");
-		plan.setStudyKey("study-key");
-		plan.setStrategy(strategy);
-		return plan;
-	}
+        // This is valid, then you can manipulate it to invalidate it.
+        SchedulePlan plan = new DynamoSchedulePlan();
+        plan.setLabel("a label");
+        plan.setStudyKey("study-key");
+        plan.setStrategy(strategy);
+        return plan;
+    }
 }
