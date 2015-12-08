e3e61f6473f2fdf5514842e63e6d0c72b60dddb9
alxdarksage <alx.dark@sagebase.org>
2015-12-08 12:37:39 -0800

    Removing Redis Key code.

--- app/org/sagebionetworks/bridge/redis/AbstractRedisKey.java
+++ app/org/sagebionetworks/bridge/redis/AbstractRedisKey.java
@@ -10,13 +10,10 @@ import org.sagebionetworks.bridge.config.Environment;
 
 abstract class AbstractRedisKey implements RedisKey {
 
-    private final BridgeConfig config =  BridgeConfigFactory.getConfig();
-    private final EnumSet<Environment> SHARED_ENV = EnumSet.of(Environment.DEV, Environment.LOCAL);
-    
     @Override
     public String getRedisKey(String key) {
         checkNotNull(key);
-        return keyForSharedRedis(key + RedisKey.SEPARATOR + getSuffix());
+        return key + RedisKey.SEPARATOR + getSuffix();
     }
 
     @Override
@@ -24,21 +21,6 @@ abstract class AbstractRedisKey implements RedisKey {
         checkNotNull(redisKey);
         String suffix = RedisKey.SEPARATOR + getSuffix();
         return redisKey.substring(0, redisKey.indexOf(suffix));
-    }
-    
-    /**
-     * If this key is being used in a shared environment (development or local, where multiple 
-     * users are using the same Redis server), suffix the key with the user's name so these 
-     * keys do not collide (particularly Travis and Heroku on development). Will not 
-     * change/break keys in staging or production.
-     * @param key
-     * @return
-     */
-    private String keyForSharedRedis(String key) {
-        if (SHARED_ENV.contains(config.getEnvironment())) {
-            return key + RedisKey.SEPARATOR + config.getUser();
-        }
-        return key;
     }
 
 }
