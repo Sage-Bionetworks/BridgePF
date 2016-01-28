eeaceb65d59d29491ae70c8c93fc2c189a85e435
alxdarksage <alx.dark@sagebase.org>
2016-01-27 16:30:58 -0800

    Removing unused imports

--- test/org/sagebionetworks/bridge/play/controllers/SurveyControllerTest.java
+++ test/org/sagebionetworks/bridge/play/controllers/SurveyControllerTest.java
@@ -8,8 +8,6 @@ import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
