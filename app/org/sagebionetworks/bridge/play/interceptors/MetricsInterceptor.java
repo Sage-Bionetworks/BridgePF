338c132dc88a74f5885181aff46dbdfd63880f76
alxdarksage <alx.dark@sagebase.org>
2016-03-17 14:33:39 -0700

    Slightly more efficient use of regexp: compile it once

--- app/org/sagebionetworks/bridge/play/interceptors/MetricsInterceptor.java
+++ app/org/sagebionetworks/bridge/play/interceptors/MetricsInterceptor.java
@@ -4,6 +4,8 @@ import static org.apache.http.HttpHeaders.USER_AGENT;
 import static org.sagebionetworks.bridge.BridgeConstants.METRICS_EXPIRE_SECONDS;
 import static org.sagebionetworks.bridge.BridgeConstants.X_FORWARDED_FOR_HEADER;
 
+import java.util.regex.Pattern;
+
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.sagebionetworks.bridge.models.Metrics;
@@ -20,6 +22,9 @@ import play.mvc.Result;
 public class MetricsInterceptor implements MethodInterceptor {
 
     private static final Logger logger = LoggerFactory.getLogger(MetricsInterceptor.class);
+    
+    private static final Pattern PARTICIPANTS_URL = Pattern.compile("(?<=(/participants/))([^/]*)");
+    private static final String EMAIL_TOKEN = ":email";
 
     @Override
     public Object invoke(MethodInvocation method) throws Throwable {
@@ -40,7 +45,7 @@ public class MetricsInterceptor implements MethodInterceptor {
         final Request request = Http.Context.current().request();
         final Metrics metrics = new Metrics(RequestUtils.getRequestId(request));
         // The only key we have for users is their email addresses, but it's sensitive. Don't log it.
-        String uri = request.path().replaceFirst("(?<=(/participants/))([^/]*)", ":email");
+        String uri = PARTICIPANTS_URL.matcher(request.path()).replaceAll(EMAIL_TOKEN);
         metrics.setMethod(request.method());
         metrics.setUri(uri);
         metrics.setProtocol(request.version());
