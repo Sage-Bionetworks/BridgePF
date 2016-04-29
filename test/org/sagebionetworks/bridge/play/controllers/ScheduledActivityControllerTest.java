85c57aba911c83dab06c26b22ccd3e962755fa02
alxdarksage <alx.dark@sagebase.org>
2016-04-29 13:29:41 -0700

    Adding tests to verify that we handle sessions missing accountCreatedOn and/or id.

--- test/org/sagebionetworks/bridge/play/controllers/ScheduledActivityControllerTest.java
+++ test/org/sagebionetworks/bridge/play/controllers/ScheduledActivityControllerTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
@@ -19,21 +20,31 @@ import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
 import org.sagebionetworks.bridge.BridgeUtils;
 import org.sagebionetworks.bridge.TestConstants;
 import org.sagebionetworks.bridge.TestUtils;
+import org.sagebionetworks.bridge.cache.CacheProvider;
+import org.sagebionetworks.bridge.dao.AccountDao;
 import org.sagebionetworks.bridge.dynamodb.DynamoScheduledActivity;
 import org.sagebionetworks.bridge.exceptions.NotAuthenticatedException;
 import org.sagebionetworks.bridge.json.BridgeObjectMapper;
 import org.sagebionetworks.bridge.models.ClientInfo;
 import org.sagebionetworks.bridge.models.CriteriaContext;
+import org.sagebionetworks.bridge.models.accounts.Account;
 import org.sagebionetworks.bridge.models.accounts.User;
 import org.sagebionetworks.bridge.models.accounts.UserSession;
 import org.sagebionetworks.bridge.models.schedules.ScheduleContext;
 import org.sagebionetworks.bridge.models.schedules.ScheduledActivity;
+import org.sagebionetworks.bridge.models.studies.Study;
 import org.sagebionetworks.bridge.play.controllers.ScheduledActivityController;
 import org.sagebionetworks.bridge.services.ScheduledActivityService;
+import org.sagebionetworks.bridge.services.StudyService;
 
 import play.mvc.Result;
 import play.test.Helpers;
@@ -43,15 +54,39 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ScheduledActivityControllerTest {
 
-    private ScheduledActivityService scheduledActivityService;
+    private static final DateTime ACCOUNT_CREATED_ON = DateTime.now();
+    
+    private static final String ID = "id";
     
     private ScheduledActivityController controller;
     
     private ClientInfo clientInfo;
 
-    ArgumentCaptor<ScheduleContext> argument = ArgumentCaptor.forClass(ScheduleContext.class);
+    @Mock
+    ScheduledActivityService scheduledActivityService;
+    
+    @Mock
+    AccountDao accountDao;
+    
+    @Mock
+    StudyService studyService;
+    
+    @Mock
+    CacheProvider cacheProvider;
+    
+    @Mock
+    Study study;
+    
+    @Mock
+    Account account;
+    
+    @Captor
+    ArgumentCaptor<ScheduleContext> contextCaptor;
+    
+    UserSession session;
     
     @Before
     public void before() throws Exception {
@@ -65,20 +100,28 @@ public class ScheduledActivityControllerTest {
         String json = BridgeObjectMapper.get().writeValueAsString(list);
         TestUtils.mockPlayContextWithJson(json);
         
-        UserSession session = new UserSession();
+        session = new UserSession();
         User user = new User();
         user.setHealthCode("BBB");
         user.setStudyKey(TestConstants.TEST_STUDY_IDENTIFIER);
         user.setDataGroups(Sets.newHashSet("group1"));
         user.setLanguages(TestUtils.newLinkedHashSet("en","fr"));
+        user.setAccountCreatedOn(ACCOUNT_CREATED_ON);
+        user.setId(ID);
         session.setUser(user);
         session.setStudyIdentifier(TestConstants.TEST_STUDY);
         
-        scheduledActivityService = mock(ScheduledActivityService.class);
         when(scheduledActivityService.getScheduledActivities(any(User.class), any(ScheduleContext.class))).thenReturn(list);
-        
+
+        doReturn(ACCOUNT_CREATED_ON).when(account).getCreatedOn();
+        doReturn(account).when(accountDao).getAccount(any(), eq(ID));
+        doReturn(study).when(studyService).getStudy(TestConstants.TEST_STUDY_IDENTIFIER);
+
         controller = spy(new ScheduledActivityController());
         controller.setScheduledActivityService(scheduledActivityService);
+        controller.setStudyService(studyService);
+        controller.setCacheProvider(cacheProvider);
+        controller.setAccountDao(accountDao);
         doReturn(session).when(controller).getAuthenticatedAndConsentedSession();
         
         clientInfo = ClientInfo.fromUserAgentCache("App Name/4 SDK/2");
@@ -150,10 +193,10 @@ public class ScheduledActivityControllerTest {
         DateTime now = DateTime.parse("2011-05-13T12:37:31.985+03:00");
         
         controller.getScheduledActivities(now.toString(), null, null);
-        verify(scheduledActivityService).getScheduledActivities(any(User.class), argument.capture());
+        verify(scheduledActivityService).getScheduledActivities(any(User.class), contextCaptor.capture());
         verifyNoMoreInteractions(scheduledActivityService);
-        assertEquals(now, argument.getValue().getEndsOn());
-        assertEquals(now.getZone(), argument.getValue().getZone());
+        assertEquals(now, contextCaptor.getValue().getEndsOn());
+        assertEquals(now.getZone(), contextCaptor.getValue().getZone());
     }
     
     @Test
@@ -165,11 +208,11 @@ public class ScheduledActivityControllerTest {
             .withHourOfDay(23).withMinuteOfHour(59).withSecondOfMinute(59).withMillisOfSecond(0);
         
         controller.getScheduledActivities(null, "+03:00", "3");
-        verify(scheduledActivityService).getScheduledActivities(any(User.class), argument.capture());
+        verify(scheduledActivityService).getScheduledActivities(any(User.class), contextCaptor.capture());
         verifyNoMoreInteractions(scheduledActivityService);
-        assertEquals(expectedEndsOn, argument.getValue().getEndsOn().withMillisOfSecond(0));
-        assertEquals(expectedEndsOn.getZone(), argument.getValue().getZone());
-        assertEquals(clientInfo, argument.getValue().getCriteriaContext().getClientInfo());
+        assertEquals(expectedEndsOn, contextCaptor.getValue().getEndsOn().withMillisOfSecond(0));
+        assertEquals(expectedEndsOn.getZone(), contextCaptor.getValue().getZone());
+        assertEquals(clientInfo, contextCaptor.getValue().getCriteriaContext().getClientInfo());
     }
     
     @SuppressWarnings("unchecked")
@@ -186,4 +229,24 @@ public class ScheduledActivityControllerTest {
         controller.getScheduledActivities(DateTime.now().toString(), null, null);
     }
     
+    @Test
+    public void oldSessionsWithIdAndNoAccountCreatedOn() throws Exception {
+        session.getUser().setAccountCreatedOn(null); // this is not currently in the session
+        
+        controller.getScheduledActivities(null, "-07:00", "3");
+        verify(scheduledActivityService).getScheduledActivities(any(), contextCaptor.capture());
+        ScheduleContext context = contextCaptor.getValue();
+        assertEquals(ACCOUNT_CREATED_ON, context.getAccountCreatedOn());
+    }
+    
+    @Test
+    public void oldSessionsWithNoIdAndNoAccountCreatedOn() throws Exception {
+        session.getUser().setAccountCreatedOn(null); // these are not currently in the session
+        session.getUser().setId(null);
+        
+        controller.getScheduledActivities(null, "-07:00", "3");
+        verify(scheduledActivityService).getScheduledActivities(any(), contextCaptor.capture());
+        ScheduleContext context = contextCaptor.getValue();
+        assertNotNull(context.getAccountCreatedOn()); // this is a timestamp, so
+    }
 }
