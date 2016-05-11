9cd6284d42157b6a7f4ab773c5391a48a8445bc1
alxdarksage <alx.dark@sagebase.org>
2016-05-11 15:09:19 -0700

    Simplifying UserSessionInfo.

--- app/org/sagebionetworks/bridge/models/accounts/UserSessionInfo.java
+++ app/org/sagebionetworks/bridge/models/accounts/UserSessionInfo.java
@@ -1,8 +1,5 @@
 package org.sagebionetworks.bridge.models.accounts;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
@@ -14,19 +11,42 @@ import org.sagebionetworks.bridge.models.subpopulations.SubpopulationGuid;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Sets;
 
 /**
- * Greatly trimmed user session object that is embedded in the initial render of the
- * web application. NOTE: very likely we will just start custom-serializing the session 
- * directly and delete this object, since we are already doing it here.
+ * A view of the user session that maintains the current user session fields and will include fields 
+ * as they are added to the StudyParticipant.
  */
 public class UserSessionInfo {
     
+    private static final String CONSENT_HISTORIES = "consentHistories";
+    private static final String ENCRYPTED_HEALTH_CODE = "encryptedHealthCode";
+    private static final String CONSENT_STATUSES = "consentStatuses";
+    private static final String CONSENTED = "consented";
+    private static final String SIGNED_MOST_RECENT_CONSENT = "signedMostRecentConsent";
+    private static final String DATA_SHARING = "dataSharing";
+    private static final String ENVIRONMENT = "environment";
+    private static final String SESSION_TOKEN = "sessionToken";
+    private static final String AUTHENTICATED = "authenticated";
+    private static final String USER_SESSION_INFO = "UserSessionInfo";
+    private static final String TYPE = "type";
+    
+    private static final String PRODUCTION = "production";
+    private static final String STAGING = "staging";
+    private static final String DEVELOP = "develop";
+    private static final String LOCAL = "local";
+    
     private static final BridgeObjectMapper MAPPER = BridgeObjectMapper.get();
-    private static final Set<String> PROHIBITED_FIELD_NAMES = Sets.newHashSet("type", "encryptedHealthCode", "consentHistories");
+    private static final Set<String> PROHIBITED_FIELD_NAMES = Sets.newHashSet(TYPE, ENCRYPTED_HEALTH_CODE, CONSENT_HISTORIES);
     private static final String USERNAME = "username";
+    private static final Map<Environment,String> ENVIRONMENTS = new ImmutableMap.Builder<Environment,String>()
+            .put(Environment.LOCAL, LOCAL)
+            .put(Environment.DEV, DEVELOP)
+            .put(Environment.UAT, STAGING)
+            .put(Environment.PROD, PRODUCTION).build();
 
     /**
      * Collapse StudyParticipant properties into the UserSessionInfo JSON so that we 
@@ -35,9 +55,20 @@ public class UserSessionInfo {
      * to a deserializer. 
      */
     public static JsonNode toJSON(UserSession session) {
-        ObjectNode node = (ObjectNode)MAPPER.valueToTree(new UserSessionInfo(session));
+        ObjectNode node = JsonNodeFactory.instance.objectNode();
+        node.put(AUTHENTICATED, session.isAuthenticated());
+        node.put(SESSION_TOKEN, session.getSessionToken());
+        node.put(ENVIRONMENT, ENVIRONMENTS.get(session.getEnvironment()));
+        node.put(DATA_SHARING, session.getParticipant().getSharingScope() != SharingScope.NO_SHARING);
         node.put(USERNAME, session.getParticipant().getEmail());
+        node.put(SIGNED_MOST_RECENT_CONSENT, ConsentStatus.isConsentCurrent(session.getConsentStatuses()));
+        node.put(CONSENTED, ConsentStatus.isUserConsented(session.getConsentStatuses()));
+        node.put(TYPE, USER_SESSION_INFO);
         
+        ObjectNode statuses = node.with(CONSENT_STATUSES);
+        for (Map.Entry<SubpopulationGuid, ConsentStatus> stats : session.getConsentStatuses().entrySet()) {
+            statuses.set(stats.getKey().getGuid(), MAPPER.valueToTree(stats.getValue()));
+        }
         ObjectNode partNode = (ObjectNode)MAPPER.valueToTree(session.getParticipant());
         for (Iterator<String> i = partNode.fieldNames(); i.hasNext();) {
             String fieldName = i.next();
@@ -56,49 +87,5 @@ public class UserSessionInfo {
             }
         }
         return node;
-    }
-    
-    private static final Map<Environment,String> ENVIRONMENTS = new HashMap<>();
-    static {
-        ENVIRONMENTS.put(Environment.LOCAL, "local");
-        ENVIRONMENTS.put(Environment.DEV, "develop");
-        ENVIRONMENTS.put(Environment.UAT, "staging");
-        ENVIRONMENTS.put(Environment.PROD, "production");
-    }
-
-    private final boolean authenticated;
-    private final String sessionToken;
-    private final String environment;
-    private final boolean dataSharing;
-    private final Map<SubpopulationGuid,ConsentStatus> consentStatuses;
-
-    private UserSessionInfo(UserSession session) {
-        checkNotNull(session);
-        this.authenticated = session.isAuthenticated();
-        this.sessionToken = session.getSessionToken();
-        this.environment = ENVIRONMENTS.get(session.getEnvironment());
-        this.consentStatuses = session.getConsentStatuses();
-        this.dataSharing = session.getParticipant().getSharingScope() != SharingScope.NO_SHARING;
-    }
-    public boolean isAuthenticated() {
-        return authenticated;
-    }
-    public Map<SubpopulationGuid,ConsentStatus> getConsentStatuses() {
-        return consentStatuses;
-    }
-    public boolean isConsented() {
-        return ConsentStatus.isUserConsented(consentStatuses);
-    }
-    public boolean isSignedMostRecentConsent() {
-        return ConsentStatus.isConsentCurrent(consentStatuses);
-    }
-    public String getSessionToken() {
-        return sessionToken;
-    }
-    public String getEnvironment() {
-        return environment;
-    }
-    public boolean getDataSharing() {
-        return dataSharing;
     }
 }
