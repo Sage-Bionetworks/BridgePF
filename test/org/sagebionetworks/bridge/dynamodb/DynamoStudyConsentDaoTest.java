39752e28d316ea21743ba911b3c4c0417bfbe1fb
alxdarksage <alx.dark@sagebase.org>
2016-07-14 17:38:56 -0700

    Removing unused imports.

--- test/org/sagebionetworks/bridge/dynamodb/DynamoStudyConsentDaoTest.java
+++ test/org/sagebionetworks/bridge/dynamodb/DynamoStudyConsentDaoTest.java
@@ -1,8 +1,6 @@
 package org.sagebionetworks.bridge.dynamodb;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
