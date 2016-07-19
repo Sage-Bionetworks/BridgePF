3489ab9a94aab2c83ba26a3153f24fdc2d457cbe
alxdarksage <alx.dark@sagebase.org>
2016-07-19 11:17:04 -0700

    Removing unused imports.

--- test/org/sagebionetworks/bridge/dynamodb/DynamoUploadDaoTest.java
+++ test/org/sagebionetworks/bridge/dynamodb/DynamoUploadDaoTest.java
@@ -2,7 +2,6 @@ package org.sagebionetworks.bridge.dynamodb;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
 
 import javax.annotation.Resource;
 
@@ -18,7 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import org.sagebionetworks.bridge.exceptions.ConcurrentModificationException;
 import org.sagebionetworks.bridge.json.BridgeObjectMapper;
 import org.sagebionetworks.bridge.models.upload.UploadRequest;
 import org.sagebionetworks.bridge.models.upload.UploadStatus;
