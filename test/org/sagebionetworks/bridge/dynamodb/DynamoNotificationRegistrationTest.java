cc4ec316b67fa568880a371d5826934de57ef5b5
Alx Dark <alx.dark@sagebase.org>
2017-01-11 16:20:43 -0800

    A little bit of test cleanup.

--- test/org/sagebionetworks/bridge/dynamodb/DynamoNotificationRegistrationTest.java
+++ test/org/sagebionetworks/bridge/dynamodb/DynamoNotificationRegistrationTest.java
@@ -14,26 +14,32 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 public class DynamoNotificationRegistrationTest {
     
-    private static final long CREATED_ON = DateTime.parse("2017-01-10T20:29:14.319Z").getMillis();
-    private static final long MODIFIED_ON = DateTime.parse("2017-01-11T20:29:14.319Z").getMillis();
+    private static final String DEVICE_ID = "aDeviceId";
+    private static final String ENDPOINT_ARN = "anEndpointARN";
+    private static final String GUID = "ABC";
+    private static final String HEALTH_CODE = "healthCode";
+    private static final String CREATED_ON_STRING = "2017-01-10T20:29:14.319Z";
+    private static final String MODIFIED_ON_STRING = "2017-01-11T20:29:14.319Z";
+    private static final long CREATED_ON = DateTime.parse(CREATED_ON_STRING).getMillis();
+    private static final long MODIFIED_ON = DateTime.parse(MODIFIED_ON_STRING).getMillis();
     
     @Test
     public void canSerialize() throws Exception {
         NotificationRegistration reg = NotificationRegistration.create();
-        reg.setHealthCode("healthCode");
-        reg.setGuid("ABC");
-        reg.setEndpointARN("anEndpointARN");
-        reg.setDeviceId("aDeviceId");
+        reg.setHealthCode(HEALTH_CODE);
+        reg.setGuid(GUID);
+        reg.setEndpointARN(ENDPOINT_ARN);
+        reg.setDeviceId(DEVICE_ID);
         reg.setOsName(OperatingSystem.ANDROID);
         reg.setCreatedOn(CREATED_ON);
         reg.setModifiedOn(MODIFIED_ON);
         
         JsonNode node = BridgeObjectMapper.get().valueToTree(reg);
-        assertEquals("ABC", node.get("guid").asText());
-        assertEquals("aDeviceId", node.get("deviceId").asText());
-        assertEquals("Android", node.get("osName").asText());
-        assertEquals("2017-01-10T20:29:14.319Z", node.get("createdOn").asText());
-        assertEquals("2017-01-11T20:29:14.319Z", node.get("modifiedOn").asText());
+        assertEquals(GUID, node.get("guid").asText());
+        assertEquals(DEVICE_ID, node.get("deviceId").asText());
+        assertEquals(OperatingSystem.ANDROID, node.get("osName").asText());
+        assertEquals(CREATED_ON_STRING, node.get("createdOn").asText());
+        assertEquals(MODIFIED_ON_STRING, node.get("modifiedOn").asText());
         assertEquals("NotificationRegistration", node.get("type").asText());
         assertEquals(6, node.size()); // and no other fields like healthCode;
         
@@ -42,9 +48,9 @@ public class DynamoNotificationRegistrationTest {
         String json = TestUtils.createJson("{'guid':'ABC','deviceId':'aDeviceId','osName':'iPhone OS'}");
         
         NotificationRegistration deser = BridgeObjectMapper.get().readValue(json, NotificationRegistration.class);
-        assertEquals("ABC", deser.getGuid());
-        assertEquals("aDeviceId", deser.getDeviceId());
-        assertEquals("iPhone OS", deser.getOsName());
+        assertEquals(GUID, deser.getGuid());
+        assertEquals(DEVICE_ID, deser.getDeviceId());
+        assertEquals(OperatingSystem.IOS, deser.getOsName());
     }
     
 }
