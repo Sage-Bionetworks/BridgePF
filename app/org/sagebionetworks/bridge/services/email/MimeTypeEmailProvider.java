35a5273e1cd1d21732b895b3f8a2a8a5846f0dd8
Alx Dark <alxdark@gmail.com>
2017-05-23 10:21:24 -0700

    Some comments added after review.

--- app/org/sagebionetworks/bridge/services/email/MimeTypeEmailProvider.java
+++ app/org/sagebionetworks/bridge/services/email/MimeTypeEmailProvider.java
@@ -4,6 +4,11 @@ import javax.mail.MessagingException;
 
 public interface MimeTypeEmailProvider {
 
+    /**
+     * Get the sender email address without any further formatting. So for example, if the provider formats the sender
+     * email as <code>"Study Name" &lt;email@email.com&gtl</code>, This method should return only
+     * <code>email@email.com</code>.
+     */
     String getPlainSenderEmail();
     
     MimeTypeEmail getMimeTypeEmail() throws MessagingException;
