98aaec8e3a48dfb6fba417ae7512edf704f90b4b
Alx Dark <alx.dark@sagebase.org>
2017-10-24 15:38:08 -0700

    Renaming this test.

--- test/org/sagebionetworks/bridge/validators/EmailVerificationValidatorTest.java
+++ test/org/sagebionetworks/bridge/validators/EmailVerificationValidatorTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.sagebionetworks.bridge.TestUtils;
 import org.sagebionetworks.bridge.models.accounts.EmailVerification;
 
-public class VerificationTokenValidatorTest {
+public class EmailVerificationValidatorTest {
     @Test
     public void verifySpToken() {
         EmailVerification token = new EmailVerification("sptoken");
@@ -14,6 +14,6 @@ public class VerificationTokenValidatorTest {
     @Test
     public void testTokenRequired() {
         EmailVerification token = new EmailVerification(null);
-        TestUtils.assertValidatorMessage(EmailVerificationValidator.INSTANCE, token, "token", "is required");
+        TestUtils.assertValidatorMessage(EmailVerificationValidator.INSTANCE, token, "sptoken", "is required");
     }
 }
