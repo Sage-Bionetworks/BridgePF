1519fdade68a8de09fc294b0414c4430891b8be0
Alx Dark <alx.dark@sagebase.org>
2018-01-09 17:39:47 -0800

    Fix persistence of email and phone sign in sessions.

--- app/org/sagebionetworks/bridge/services/AuthenticationService.java
+++ app/org/sagebionetworks/bridge/services/AuthenticationService.java
@@ -412,7 +412,8 @@ public class AuthenticationService {
         accountDao.verifyChannel(channelType, account);
 
         UserSession session = getSessionFromAccount(study, context, account);
-
+        cacheProvider.setUserSession(session);
+        
         if (!session.doesConsent() && !session.isInRole(Roles.ADMINISTRATIVE_ROLES)) {
             throw new ConsentRequiredException(session);
         }
