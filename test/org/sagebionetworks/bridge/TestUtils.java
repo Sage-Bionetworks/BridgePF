61ec213ef4e3bc15f2086f9a24af0a07df8fcd4f
Alx Dark <alx.dark@sagebase.org>
2018-02-07 13:18:44 -0800

    Removing sop.

--- test/org/sagebionetworks/bridge/TestUtils.java
+++ test/org/sagebionetworks/bridge/TestUtils.java
@@ -120,7 +120,6 @@ public class TestUtils {
             Validate.entityThrowingException(validator, object);
             fail("Should have thrown exception");
         } catch(InvalidEntityException e) {
-            System.out.println(e.getErrors());
             if (e.getErrors().get(fieldName).contains(fieldNameAsLabel+error)) {
                 return;
             }
