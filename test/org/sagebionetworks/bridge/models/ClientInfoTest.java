84255221000802709d90c4ac4fabbd345a15c6e4
Alx Dark <alx.dark@sagebase.org>
2018-03-08 13:53:38 -0800

    Sorry... keep thinking up new tests. This one also makes sense.

--- test/org/sagebionetworks/bridge/models/ClientInfoTest.java
+++ test/org/sagebionetworks/bridge/models/ClientInfoTest.java
@@ -63,6 +63,7 @@ public class ClientInfoTest {
         assertClientInfo("/app (iOS) 10", null, null, null, "iPhone OS", null, null, 10);
         assertClientInfo("/app", null, null, null, null, null, null, null);
         assertClientInfo("10//10", null, null, null, null, null, null, null);
+        assertClientInfo("10 10", null, 10, null, null, null, null, 10);
         assertClientInfo("/7 (Android/10) /5", null, 7, null, "Android", "10", null, 5);
         assertClientInfo("app/3 (Device; /2.0.0) sdk/4", "app", 3, "Device", null, "2.0.0", "sdk", 4);
         assertClientInfo("AppName/1 (Device Name; iPhone OS) BridgeJavaSDK/3",
